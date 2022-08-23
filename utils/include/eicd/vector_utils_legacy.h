94890073ac2f866d62dd6078705648b2c092b5e0
Wouter Deconinck <wdconinc@gmail.com>
2022-08-23 14:34:07 +0000

    remove eicd::Vector2f and eicd::Vector3f

--- utils/include/eicd/vector_utils_legacy.h
+++ utils/include/eicd/vector_utils_legacy.h
@@ -10,8 +10,6 @@
 
 #include <edm4hep/Vector2f.h>
 #include <edm4hep/Vector3f.h>
-#include <eicd/Vector2f.h>
-#include <eicd/Vector3f.h>
 
 namespace eicd {
 
@@ -28,14 +26,6 @@ template <class V> auto vector_y(const V& v) { return v.y; }
 template <class V> auto vector_z(const V& v) { return v.z; }
 // Vector2f uses a,b instead of x,y
 template <>
-inline auto vector_x<eicd::Vector2f>(const eicd::Vector2f& v) {
-  return v.a;
-}
-template <>
-inline auto vector_y<eicd::Vector2f>(const eicd::Vector2f& v) {
-  return v.b;
-}
-template <>
 inline auto vector_x<edm4hep::Vector2f>(const edm4hep::Vector2f& v) {
   return v.a;
 }
@@ -44,10 +34,6 @@ inline auto vector_y<edm4hep::Vector2f>(const edm4hep::Vector2f& v) {
   return v.b;
 }
 // no z-component for 2D vectors
-template <>
-inline auto vector_z<eicd::Vector2f>(const eicd::Vector2f& v) {
-  return 0;
-}
 template <>
 inline auto vector_z<edm4hep::Vector2f>(const edm4hep::Vector2f& v) {
   return 0;
